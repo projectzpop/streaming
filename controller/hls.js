737a180c5191494c5292ed190bcbf4352bba4784
Sam (Sangho Kim) <rlaace423@gmail.com>
2019-03-26 22:02:48 +0900

    미사용 소스 정리

--- controller/hls.js
+++ controller/hls.js
@@ -32,66 +32,9 @@ const example = (req, res) => {
     res.status(200).set('Content-Type', 'application/vnd.apple.mpegurl');
     fs.createReadStream(filePath).pipe(res);
   }
-  // const target = 'test';
-  // const filePath = `${path.resolve(audioRoot, target)}/${req.params[0]}`;
-  // console.log(filePath);
-  // ... Here you would do some Auth (userId, sessionId/cookie info validation)
-
-  //
-  // fs.stat(filePath, (err, stats) => {
-  //   if (!stats || err) {
-  //     res.status(404).end();
-  //   } else {
-  //     let fileExt = filePath.slice(filePath.lastIndexOf('.'));
-  //     fileExt = fileExt && fileExt.toLowerCase() || fileExt;
-  //   }
-  // });
-  //
-  //
-  // switch (fileExt) {
-  //   case '.m3u8':
-  //     res.status(200).set('Content-Type', 'application/vnd.apple.mpegurl');
-  //     target = `${req.params[0]}`;
-  //     filePath = `${path.resolve(audioRoot, target)}/${target}.m3u8`;
-  //     fs.createReadStream(filePath).pipe(res);
-  //     break;
-  //   case '.ts':
-  //     res.status(200).set('Content-Type', 'video/MP2T');
-  //     fs.createReadStream(filePath).pipe(res);
-  //     break;
-  //   default:
-  //     res.status(400).send(`Unexpected file type ${fileExt}`);
-  // }
-
-};
-
-const getM3u8 = (req, res) => {
-  console.log('getM3u8');
-
-  // TODO: DB 연동
-  const target = 'test';
-  res.status(200).set('Content-Type', 'application/vnd.apple.mpegurl');
-
-  const filePath = `${path.resolve(audioRoot, target)}/${target}.m3u8`;
-  console.log(filePath);
-  fs.createReadStream(filePath).pipe(res);
-};
-
-const getTs = (req, res) => {
-  console.log('getTs');
-
-  // TODO: DB 연동
-  const target = 'test';
-  res.status(200).set('Content-Type', 'video/MP2T');
-
-  const filePath = `${path.resolve(audioRoot, target)}/${req.params[0]}`;
-  console.log(filePath);
-  fs.createReadStream(filePath).pipe(res);
 };
 
 
 module.exports = {
-  getM3u8,
-  getTs,
   example,
 };
